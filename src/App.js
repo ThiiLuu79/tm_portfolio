ed11389cbac80c50824b2408b77f77ddaab9ebe0
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-04-13 19:06:33 -0400

    Add Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+
+import './css/App.css';
+import './css/custom.css';
+import './css/general.css';
+import './css/style.css';
+
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+
+        
+
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+
+    return (
+        <header id="top">
+            <h3 class = "header__title tm">tm's <strong class="portfolio"><b>portfolio</b></strong> </h3>
+        </header>
+    )
+}
+export default Header
