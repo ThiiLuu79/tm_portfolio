24875483dc8d9b48bcacc6d7391f507870220678
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-04-14 14:37:24 -0400

    Removed Navbar

--- src/components/MainComponents/Header.js
+++ src/components/MainComponents/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header id="top">
             <h3 class = "header__title tm">tm's <strong class="portfolio"><b>portfolio</b></strong> </h3>
-            <Navbar></Navbar>
+            
         </header>
     )
 }
