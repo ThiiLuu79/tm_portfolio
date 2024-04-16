828ddd565d6194377c132d8e0b6b2fd2fe2f236e
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-04-16 10:11:55 -0400

    Updated GoToTop component

--- src/components/MainComponents/GoToTop.jsx
+++ src/components/MainComponents/GoToTop.jsx
@@ -1,9 +1,17 @@
+import {Link} from 'react-router-dom';
+
 const GoToTop = () => {
 
     return (
-        <a href="#top" className="go__top">
+        <Link className="go__top" onClick={() => {
+            window.scroll({
+              top: 0,
+              left: 0,
+              behavior: "smooth",
+            });
+          }}>
             <div className="button__arrow"></div>
-        </a>
+        </Link>
     )
 }
 export default GoToTop
