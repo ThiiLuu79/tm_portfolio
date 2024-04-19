29b748895670317b63c1adb34ba12cf8ece1d60d
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-04-19 13:09:44 -0400

    Added home redirecting on NotFound

--- src/components/MainComponents/NotFound.jsx
+++ src/components/MainComponents/NotFound.jsx
@@ -1,11 +1,12 @@
 import GoBack from '../MainComponents/GoBack';
+import {Link} from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <div>
             <GoBack/>
             <h3 className="page_not_found">
-                Whoops! It seems we've hit a dead end. Let's get back on track. Navigate back to the homepage to continue your exploration. See you there!
+                Whoops! It seems we've hit a dead end. Let's get back on track. <Link to="/">Navigate back to the homepage to continue your exploration</Link>. See you there!
             </h3>
         </div>
 
