2099b52b4906e3d9f0bd9181953ac01649f8bf57
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-04-19 21:24:32 -0400

    Displaying error code for NotFound

--- src/components/MainComponents/NotFound.jsx
+++ src/components/MainComponents/NotFound.jsx
@@ -5,9 +5,14 @@ const NotFound = () => {
     return (
         <div>
             <GoBack/>
-            <h3 className="page_not_found">
-                Whoops! It seems we've hit a dead end. Let's get back on track. <Link to="/">Navigate back to the homepage to continue your exploration</Link>. See you there!
-            </h3>
+            <div className="page_not_found">
+                <h2>404</h2>
+                <br/>
+                <h3 >
+                    Whoops! It seems we've hit a dead end. Let's get back on track. <Link to="/">Navigate back to the homepage to continue your exploration</Link>. See you there!
+                </h3>
+            </div>
+
         </div>
 
     )
