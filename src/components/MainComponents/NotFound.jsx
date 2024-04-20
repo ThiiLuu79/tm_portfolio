c1b0826d1543c0c6e23cd180656b6ef28e06c732
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-04-19 21:34:45 -0400

    Updated NotFound component

--- src/components/MainComponents/NotFound.jsx
+++ src/components/MainComponents/NotFound.jsx
@@ -9,7 +9,9 @@ const NotFound = () => {
                 <h2>404</h2>
                 <br/>
                 <h3 >
-                    Whoops! It seems we've hit a dead end. Let's get back on track. <Link to="/">Navigate back to the homepage to continue your exploration</Link>. See you there!
+                    Whoops! It seems we've hit a dead end. Let's get back on track. <Link to="/" style={{ textDecoration: 'underline' }}>Navigate back to the homepage to continue your exploration</Link>.<br/><br/>
+                    You can still use the "Go back to home page" button or the navbar to navigate back to the homepage.<br/><br/>
+                    See you there!
                 </h3>
             </div>
 
