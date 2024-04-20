d5141631bf37001559f2ec6222c8a323375d503b
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-04-20 08:49:49 -0400

    Added WorkExperience component

---
+++ src/components/ReusableComponents/WorkExperience.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const WorkExperience = ({ title, image, imgAlt, company, companyLink, location, workTime, description, contributions, tools }) => (
+    
+    <section className="block project__item">
+        <div className = "block__body">
+            <div className="job-details">
+                <h3>{title}</h3>
+                <img src = {image} alt={imgAlt} width="500" className="block__img"></img>
+                <p><a href={companyLink} target="_blank" rel="noreferrer">{company}</a> - {location}</p>
+                <p id="nbc_time">{workTime}</p>
+            </div>
+
+            <div className="job-summary">
+                <p>{description}</p>
+            </div>
+            <br></br>
+            <p className="cont-title">Key contributions:</p>
+            <ul className="cont">
+                {contributions.map((contribution) => (
+                    <li key={contribution}>{contribution}</li>
+                ))}
+            </ul>
+            <div className="environment">
+                <p className="tools">Tools</p>
+                <ul className="tool-list">
+                    {tools.map((tool) => (
+                        <li key={tool}>{tool}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    </section>
+    
+);
+
+export default WorkExperience;
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,10 @@
 import GoBack from './MainComponents/GoBack';
+import WorkExperience from "./ReusableComponents/WorkExperience";
+
 import cover from "../img/experiences/experience.jpg";
 import nbcImg from "../img/experiences/nbc_interns.jpg";
+import inlibro from "../img/experiences/inlibro.png";
+import ciuss from "../img/experiences/ciuss.jpg";
 
 import getExpTimeString from "../js/time.js";
 
@@ -29,104 +33,45 @@ const Work = () => {
 
             <div className="block_wrap">
 
-                <section className="block project__item">
-                    <div className = "block__body">
-                    <div className="job-details">
-                        <h3>Full Stack Developer (Internship)</h3>
-                        <img src = {nbcImg} alt="nbc interns" width="500" className="block__img"></img>
-                        <p><a href="https://www.bnc.ca/" target="_blank" rel="noreferrer">National Bank of Canada (NBC)</a> - Montreal, Canada</p>
-                        <p id="nbc_time">{nbcExpTimeString}</p>
-                    </div>
+                <WorkExperience
+                    title={"Full Stack Developer (Internship)"}
+                    image={nbcImg}
+                    imgAlt={"nbc interns"}
+                    company={"National Bank of Canada (NBC)"}
+                    companyLink={"https://www.bnc.ca/"}
+                    location={"Montreal, Canada"}
+                    workTime={nbcExpTimeString}
+                    description={"Worked as a Full Stack Developer at National Bank of Canada. Contributed in the redesign of the transactional site (DBE project)."}
+                    contributions={["Joined the Vikings team. Worked on the hybrid navigation between the older and newer interface.", "Implemented new features to enhance the interface.", "Fixed defects on the interface.", "Analyzed the data and raised anomalies in a SQL report.", "Created flat tables to store data from multiple systems."]}
+                    tools={["Environment: MacOS, Windows", "Database: Oracle", "Languages: Java, JavaScript, Java Server Page (JSP), HTML, SQL", "Methodology: Agile, Kanban", "Others: IntelliJ, Git, JUnit, Bitbucket, Jira, Confluence, Maven, DBeaver, Docker, Websphere Application Server, Urban Code Deploy, Jenkins, DataHub, Snowflake, AWS Cloud, Teams, Slack"]}
+                />
 
-                    <div className="job-summary">
-                        <p>Worked as a Full Stack Developer at National Bank of Canada. Contributed in the redesign of the transactional site (DBE project).</p>
-                    </div>
-                    <br></br>
-                    <p className="cont-title">Key contributions:</p>
-                    <ul className="cont">
-                        <li>Joined the Vikings team. Worked on the hybrid navigation between the older and newer interface.</li>
-                        <li>Implemented new features to enhance the interface.</li>
-                        <li>Fixed defects on the interface.</li>
-                        <li>Analyzed the data and raised anomalies in a SQL report.</li>
-                        <li>Created flat tables to store data from multiple systems.</li>
-                    </ul>
-                    <div className="environment">
-                        <p className="tools">Tools</p>
-                        <ul className="tool-list">
-                        <li><b>Environment</b>: MacOS, Windows</li>
-                        <li><b>Database</b> : Oracle</li>
-                        <li><b>Languages</b>: Java, JavaScript, Java Server Page (JSP), HTML, SQL</li>
-                        <li><b>Methodology</b>: Agile, Kanban</li>
-                        <li><b>Others</b>: IntelliJ, Git, JUnit, Bitbucket, Jira, Confluence, Maven, DBeaver, Docker, Websphere Application Server, Urban Code Deploy, Jenkins, DataHub, Snowflake, AWS Cloud, Teams, Slack</li>
-                        </ul>
-                    </div>
-                    </div>
-                </section>
+                <WorkExperience
+                    title={"Full Stack Developer (Internship)"}
+                    image={inlibro}
+                    imgAlt={"inlibro"}
+                    company={"Solution inLibro"}
+                    companyLink={"https://inlibro.com/"}
+                    location={"Montreal, Canada"}
+                    workTime={inlibroExpTimeString}
+                    description={"Worked as a Full Stack Developer on Koha, an open source library management system distributed in different schools and libraries."}
+                    contributions={["Fixed bugs on Koha’s UI and UX", "Created patches to improve Koha’s system", "Rebased old patches to make them work on the current Koha system", "Interacted with Koha community to solve problems", "Personalized the client’s interface for different schools", "Worked on inLibro’s plugins, which can be used in Koha", "Maintenance", "Unit testing", "QA testing", "Assisted new interns"]}
+                    tools={["Environment: Linux, Koha", "Database: MariaDB", "Languages: Perl, CSS, JavaScript, SQL", "Methodology: Agile, Kanban", "Others: Git, Apache, Bugzilla"]}
+                />
 
-                <section className="block project__item">
-                    <div className = "block__body">
-                    <div className="job-details">
-                        <h3>Full Stack Developer (Internship)</h3>
-                        <p><a href="https://inlibro.com/" target="_blank" rel="noreferrer">Solution inLibro</a> - Montreal, Canada</p>
-                        <p id="inlibro_time">{inlibroExpTimeString}</p>
-                    </div>
-
-                    <div className="job-summary">
-                        <p>Worked as a Full Stack Developer on Koha, an open source library management system distributed in different schools and libraries.</p>
-                        <br></br>
-                        <p className="cont-title">Key contributions:</p>
-                        <ul className="cont">
-                        <li>Fixed bugs on Koha’s UI and UX</li>
-                        <li>Created patches to improve Koha’s system</li>
-                        <li>Rebased old patches to make them work on the current Koha system</li>
-                        <li>Interacted with Koha community to solve problems</li>
-                        <li>Personalized the client’s interface for different schools</li>
-                        <li>Worked on inLibro’s plugins, which can be used in Koha</li>
-                        <li>Maintenance</li>
-                        <li>Unit testing</li>
-                        <li>QA testing</li>
-                        <li>Assisted new interns</li>
-                        </ul>
-                    </div>
-                    <div className="environment">
-                        <br></br>
-                        <p className="tools">Tools</p>
-                        <ul className="tool-list">
-                        <li><b>Environment</b>: Linux, Koha</li>
-                        <li><b>Database</b>: MariaDB</li>
-                        <li><b>Languages</b>: Perl, CSS, JavaScript, SQL</li>
-                        <li><b>Methodology</b>: Agile, Kanban</li>
-                        <li><b>Others</b>: Git, Apache, Bugzilla</li>
-                        </ul>
-                    </div>
-                    <br></br>
-                    </div>
-                </section>
-
-                <section className="block project__item">
-                    <div className = "block__body">
-                    <div className="job-details">
-                        <h3>Food Attendant</h3>
-                        <p><a href="https://ciusss-estmtl.gouv.qc.ca/" target="_blank" rel="noreferrer">Residential and long-term care centers (CHSLD)</a> - Montreal, Canada</p>
-                        <p id="ciuss_time">{ciussExpTimeString}</p>
-                    </div>
-
-                    <div className="job-summary">
-                        <p>Worked as a Food Attendant in multiple care centers, such as CHSLD Nicolet and CHSLD J-H Charbonneau.</p>
-                        <br></br>
-                        <p className="cont-title">Key contributions:</p>
-                        <ul className="cont">
-                        <li>Prepared meal trays for elderly residents</li>
-                        <li>Offered services to elderly residents during breakfast, lunch, and dinner</li>
-                        <li>Maintained and cleaned the kitchen</li>
-                        <li>Delivered meals to elderly residents’ rooms</li>
-                        <li>Made the inventory of food items in the kitchen</li>
-                        </ul>
-                    </div>
-                    <br></br>
-                    </div>
-                </section>
-                </div>
+                <WorkExperience
+                    title={"Food Attendant"}
+                    image={ciuss}
+                    imgAlt={"ciuss"}
+                    company={"Residential and long-term care centers (CHSLD)"}
+                    companyLink={"https://ciusss-estmtl.gouv.qc.ca/"}
+                    location={"Montreal, Canada"}
+                    workTime={ciussExpTimeString}
+                    description={"Worked as a Food Attendant in multiple care centers, such as CHSLD Nicolet and CHSLD J-H Charbonneau."}
+                    contributions={["Prepared meal trays for elderly residents", "Offered services to elderly residents during breakfast, lunch, and dinner", "Maintained and cleaned the kitchen", "Delivered meals to elderly residents’ rooms", "Made the inventory of food items in the kitchen"]}
+                    tools={["Meal Delivery Carts", "Kitchen Appliances", "Cleaning Equipment", "Inventory Management"]}
+                />
+            </div>
             
         </div>
     )
