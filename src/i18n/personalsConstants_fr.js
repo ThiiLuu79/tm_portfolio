c08e2ac7bf0f59eef32ac36e1c18e4fd0eab85b9
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-07-08 00:10:03 -0400

    Added translation for personnals

---
+++ src/i18n/personalsConstants_fr.js
@@ -0,0 +1,66 @@
+// Constante pour Personals.jsx
+export const PERSONALS_WELCOME = "Voici mes ";
+export const PERSONALS_PROJECT = "Projets Personnels";
+
+
+// PROJET CSS ZEN GARDEN
+export const CSS_ZEN_GARDEN_PROJECT = {
+    title: "Jardin Zen CSS",
+    period: "2021",
+    description: "Pour améliorer mes compétences en CSS, j'ai décidé de relever le 'Défi du Jardin Zen CSS'. La tâche consistait à styliser une page web commune en utilisant uniquement du CSS, sans modifier le HTML original. Cela s'est avéré être un projet cool qui m'a aidé à apprendre différentes astuces CSS. J'étais fier de ce que j'avais accompli, et c'était intéressant de voir comment d'autres développeurs ont abordé le même défi. Dans l'ensemble, c'était une manière amusante pour moi d'explorer et d'améliorer mes compétences en CSS.",
+    contributions: [
+        "Stylisé une page web commune en utilisant uniquement du CSS, sans changer le HTML original"
+    ],
+    workTime: "1 semaine",
+    tools: [
+        "Langue(s) : CSS",
+        "Outil(s) : Git/GitHub, VSCode"
+    ],
+    grades: [
+        []
+    ],
+    imgAlt: "jardinZenCSS"
+};
+
+// PROJET PYGAME
+export const PYGAMES_PROJECT = {
+    title: "Projets Pygame",
+    period: "2021",
+    description: "En utilisant le module de jeu de Python, Pygame, j'ai affiné mes compétences en programmation en créant une variété de jeux captivants. Parmi mes créations figurent des titres comme Space Invaders, Car Crash et Hungry Shark.",
+    contributions: [
+        "Créé une variété de jeux captivants en utilisant le module de jeu de Python, Pygame",
+        "Créé des titres comme Space Invaders, Car Crash et Hungry Shark"
+    ],
+    workTime: "1 semaine par jeu",
+    tools: [
+        "Langue(s) : Python / Pygame",
+        "Outil(s) : Git/GitHub, Pycharm"
+    ],
+    grades: [
+        []
+    ],
+    imgAlt: "pygames"
+};
+
+// PROJET TITITEA
+export const TITITEA_PROJECT = {
+    title: "Tititea",
+    period: "2021",
+    description: "Pour mon premier site web, j'ai créé une page pour TiTiTea, un salon de thé fictif. Sur cette page, les gens pouvaient commander du thé, donner leur avis et même consulter les offres d'emploi. En travaillant dessus, j'ai acquis des compétences utiles en développement web. J'ai commencé par rendre la page attrayante et facile à utiliser. Ensuite, j'ai ajouté des fonctionnalités pour aider les utilisateurs, comme des messages qui apparaissent s'ils oublient de remplir toutes les informations requises dans un formulaire. Pour finir, j'ai mis en place un système à l'arrière-plan pour stocker en toute sécurité les informations saisies par les utilisateurs dans une base de données. Les utilisateurs recevaient un message de confirmation rapide, les informant que leur demande avait été envoyée avec succès à l'arrière-plan.",
+    contributions: [
+        "Créé une page pour TiTiTea, un salon de thé fictif",
+        "Les utilisateurs pouvaient commander du thé, donner leur avis et consulter les offres d'emploi",
+        "Ajouté des fonctionnalités pour aider les utilisateurs, comme des messages qui apparaissent s'ils oublient de remplir toutes les informations requises dans un formulaire",
+        "Mis en place un système à l'arrière-plan pour stocker en toute sécurité les informations saisies par les utilisateurs dans une base de données",
+        "Les utilisateurs recevaient un message de confirmation rapide, les informant que leur demande avait été envoyée avec succès à l'arrière-plan"
+    ],
+    workTime: "2 semaines",
+    tools: [
+        "Langue(s) : HTML, CSS, JavaScript",
+        "Outil(s) : Git/GitHub, VSCode"
+    ],
+    grades: [
+        []
+    ],
+    imgAlt: "tititea"
+};
