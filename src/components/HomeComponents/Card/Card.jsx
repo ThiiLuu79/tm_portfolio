265be067da504518313e9381a6d28080a8d14ced
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-09-10 14:36:47 -0400

    Fix

--- src/components/HomeComponents/Card/Card.jsx
+++ src/components/HomeComponents/Card/Card.jsx
@@ -2,7 +2,7 @@ const Card = ({ intro, title, subtitle, image  }) => {
 
     return (
         <section className="intro">
-            <h2 className = "section__title section__title--intro">{intro} <strong className="card-title"><b>{title}</b></strong> </h2>
+            <h2 className = "section__title section__title--intro">{intro} <strong><b>{title}</b></strong> </h2>
             <p className = "section__subtitle section__subtitle--intro user_title">{subtitle}</p>
             <img src = {image} alt="card" className="card__img"></img>
         </section>
