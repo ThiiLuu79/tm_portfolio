97acc996cdb95dfe089a998a8a9fe0ec354a56af
The Minh Luong <81328535+ThiiLuu79@users.noreply.github.com>
2024-09-11 22:42:19 -0400

    Fix

--- src/components/MainComponents/NotFound/NotFound.jsx
+++ src/components/MainComponents/NotFound/NotFound.jsx
@@ -11,14 +11,16 @@ const NotFound = () => {
     const mainConstants = language === 'en' ? mainConstantsEn : mainConstantsFr;
 
     return (
-        <div className="page_not_found">
-            <h2>{mainConstants.ERROR_404}</h2>
-            <br/>
-            <h3 >
-                {mainConstants.ERROR_MESSAGE} <Link to="/" style={{ textDecoration: 'underline' }}>{mainConstants.NAVIGATE_BACK}</Link>.<br/><br/>
-                {mainConstants.HINTS}<br/><br/>
-                {mainConstants.SEE_YOU}
-            </h3>
+        <div className='not_found_container'>
+            <div className="page_not_found">
+                <h2>{mainConstants.ERROR_404}</h2>
+                <br/>
+                <h3 >
+                    {mainConstants.ERROR_MESSAGE} <Link to="/" style={{ textDecoration: 'underline' }}>{mainConstants.NAVIGATE_BACK}</Link>.<br/><br/>
+                    {mainConstants.HINTS}<br/><br/>
+                    {mainConstants.SEE_YOU}
+                </h3>
+            </div>
         </div>
     )
 }
